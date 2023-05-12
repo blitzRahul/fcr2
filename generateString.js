6ee381eec1db54c6228ef1c59f0490c2b67d23c1
blitzRahul <104504393+blitzRahul@users.noreply.github.com>
2023-05-12 15:56:21 +0530

    Create generateString.js

---
+++ generateString.js
@@ -0,0 +1,11 @@
+module.exports= (length)=>{
+
+    //this function was copied to make the development process faster
+    const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        let result = '';
+        const charactersLength = characters.length;
+        for ( let i = 0; i < length; i++ ) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }   
+        return result;
+}
